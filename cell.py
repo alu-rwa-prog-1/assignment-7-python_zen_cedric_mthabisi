21c79ec9db6c7f68fe1a08b5e24a4bf6349319e2
Mthabisi Ndlovu <m.ndlovu@alustudent.com>
2020-11-16 00:01:03 +0200

    Added pseudo code and tests for the Cell class

---
+++ cell.py
@@ -0,0 +1,36 @@
+"""
+Authors: Cedric Murairi & Mthabisi Ndlovu
+
+This program describes the Cell class.
+
+Cell
+-> Instance Variables
+    - status
+    - color
+
+-> Methods
+    - set_dead
+    - set_alive
+    - is_alive
+"""
+
+# define Cell class
+# initialize cell
+# set status to dead
+# set color to black (default color of the board)
+
+# set_dead
+# set status to dead
+# set color to black
+
+# set_alive
+# set status to alive
+# set color to green
+
+# is_alive
+# return true if status is alive, otherwise return false
+
+# Tests
+
+# Test Number | Test Type |  Test Data  |   Reason    | Expected Outcome | Actual Outcome | Pass/Fail
+#     1          Valid     status=dead    set_dead method
